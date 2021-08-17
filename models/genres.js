1465b06c9a5879869181d2135064c812cb09a9ef
haroon-fayyaz-qbatch <haroon.fayyaz@qbatch.com>
2021-08-17 17:42:47 +0500

    genres table created

---
+++ models/genres.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class genres extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  genres.init({
+    gen_title: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'genres',
+    underscored: true,
+  });
+  return genres;
+};
